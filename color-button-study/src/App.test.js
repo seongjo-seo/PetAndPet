381abb7c9a22450cb477ca677ceebfc710767d3b
seoseongjo <tjtjdwh5321@gmail.com>
2023-05-23 16:11:14 +0900

    fix: 카멜케이스 적용시 backgroundColor가 거짓 통과가 발생함. "background-color"로 수정.

--- color-button-study/src/App.test.js
+++ color-button-study/src/App.test.js
@@ -6,5 +6,6 @@ test('버튼이 올바른 초기 색상을 가지고 있는 경우. button has c
 
   const colorButton = screen.getByRole('button', { name: 'Change to blue color'});
 
-  expect(colorButton).toHaveStyle({backgroundColor: 'purple'})
+  // expect the background color to be purple
+  expect(colorButton).toHaveStyle({"background-color": "purple"})
 });
