0770c1da82dbe22b544ca4ad49496c88e37fa299
seoseongjo <tjtjdwh5321@gmail.com>
2023-05-23 16:07:44 +0900

    feat: Jest를 활용한 첫 Test case 작성. case) 초기 색상이 purple 색인 경우

--- color-button-study/src/App.test.js
+++ color-button-study/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('버튼이 올바른 초기 색상을 가지고 있는 경우. button has correct initail color', () => {
+  render( <App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue color'});
+
+  expect(colorButton).toHaveStyle({backgroundColor: 'purple'})
 });
