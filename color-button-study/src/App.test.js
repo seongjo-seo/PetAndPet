5d5a967791fe2487c7eda4d216bb2ac26942c15a
seoseongjo <tjtjdwh5321@gmail.com>
2023-05-24 22:48:10 +0900

    test: log Roles 기초 테스트

--- color-button-study/src/App.test.js
+++ color-button-study/src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { logRoles } from '@testing-library/dom';
+
 test('버튼이 올바른 초기 색상을 가지고 있는 경우. button has correct initail color', () => {
-  render( <App />);
+  const {container} = render( <App />);
+  logRoles(container);
 
   const colorButton = screen.getByRole('button', { name: 'Change to blue color'});
 
